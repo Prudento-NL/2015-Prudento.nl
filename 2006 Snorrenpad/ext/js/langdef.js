02d4800873eb2b90732c95b2b36978d7d5a57945
Kees van Leeuwen <git@prudento.nl>
2017-04-09 08:46:58 +0200

    Secure the code of the 'old' website

---
+++ 2006 Snorrenpad/ext/js/langdef.js
@@ -0,0 +1,17 @@
+////////////////////////////////////////////////
+//
+// Porta text resources
+//
+// Language (in English) : English
+//
+// ISO 3166 country code : UK
+//
+// Translation by        : Mikkel B. Stegmann
+//
+////////////////////////////////////////////////
+var ls_previous   = 'Previous';
+var ls_next       = 'Next';
+var ls_home       = 'Home';
+var ls_showall    = 'Show all';
+var ls_lightbox   = 'Lightbox';
+var ls_albumview  = 'Album view';
